23949bcd4c438063e94cf5e587b5d3af5c777bb3
{predo80} <{predrag.vuckovic80@gmail.com}>
2023-01-16 16:07:05 +0100

    update heading text

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -8,7 +8,7 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>Welcome to ReactMeals</h1>
+        <h1>Welcome to ReactMeals- Recipes</h1>
         <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
